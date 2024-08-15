15d2ee31034575ee5c506628fd09c1522503e320
prak96 <p@gmail.com>
2024-08-15 19:41:55 +0530

    pushing remaining uncomit files

--- scripts/webpools.ps1
+++ scripts/webpools.ps1
@@ -1,11 +1,11 @@
-$TempFile = "D:\Devops Practice\DevOpsPipelining\MyCalculatorApp\scripts\multivm.json"
-$TempParaFile = "D:\Devops Practice\DevOpsPipelining\MyCalculatorApp\scripts\multivm.parameter.json"
-$rg = 'webpool-rg'
+$TempFile = "D:\Devops Practice\DevOpsPipelining\MyCalculatorApp\scripts\multiwebapp.json"
+$TempParaFile = "D:\Devops Practice\DevOpsPipelining\MyCalculatorApp\scripts\multiwebapp.parameter.json"
+$rg = 'autosite-rg'
 #$loc = 'eastasia'
 
 #New-AzResourceGroup -Name $rg -Location $loc
 New-AzResourceGroupDeployment `
--Name 'deployvm1' `
+-Name 'deployapps' `
 -ResourceGroupName $rg `
 -TemplateFile $TempFile `
 -TemplateParameterFile $TempParaFile -Verbose
