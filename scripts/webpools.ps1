b1a9c54c39e425c915ce57cdcf8ac1e8a7c8a517
prak96 <p@gmail.com>
2024-08-15 19:44:33 +0530

    commit changes to stash files

--- scripts/webpools.ps1
+++ scripts/webpools.ps1
@@ -1,9 +1,16 @@
+<<<<<<< Updated upstream
 $TempFile = "D:\Devops Practice\DevOpsPipelining\MyCalculatorApp\scripts\multiwebapp.json"
 $TempParaFile = "D:\Devops Practice\DevOpsPipelining\MyCalculatorApp\scripts\multiwebapp.parameter.json"
 $rg = 'autosite-rg'
 #$loc = 'eastasia'
+=======
+$TempFile = "D:\Devops Practice\DevOpsPipelining\MyCalculatorApp\scripts\multivm.json"
+$TempParaFile = "D:\Devops Practice\DevOpsPipelining\MyCalculatorApp\scripts\multivm.parameter.json"
+$rg = 'webpool-rg'
+$loc = 'eastasia'
+>>>>>>> Stashed changes
 
-#New-AzResourceGroup -Name $rg -Location $loc
+New-AzResourceGroup -Name $rg -Location $loc
 New-AzResourceGroupDeployment `
 -Name 'deployapps' `
 -ResourceGroupName $rg `
